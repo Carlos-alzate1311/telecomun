206beddb4021b7a6442c6fef5bb248aaadbaff0b
carlos-2022 <carlos.alzateg131122@gmail.com>
2023-07-18 22:27:34 -0500

    carga inical

---
+++ src/router/App.js
@@ -0,0 +1,11 @@
+
+import '../css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Navegacion} from '../layouts/Navegacion'
+function App() {
+  return (
+    <Navegacion/>
+  );
+}
+
+export default App;
